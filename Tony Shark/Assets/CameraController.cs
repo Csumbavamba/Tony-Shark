2ac85685bd63ec1ffbc881e23b700ca3d10258e2
louis <louiscresswell33@gmail.com>
2019-03-06 17:46:43 +1300

    Player Movement Added

---
+++ Tony Shark/Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Transform objectToFollow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectToFollow = GetComponentInParent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = objectToFollow.position;
+        this.transform.up = Vector3.up;
+    }
+}
--- Tony Shark/Assets/Prefabs/MainCharacter/PlayerControl.cs
+++ Tony Shark/Assets/Prefabs/MainCharacter/PlayerControl.cs
@@ -1,29 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerControl : MonoBehaviour
 {
     private Camera myCamera;
-    private GameObject player;
+    private GameObject tony;
     private GameObject shark;
+    private CharacterController characterController;
+
+    public Slider balanceSlider;
 
     //-----Input
     private float balanceInput;
 
     //-----Movement
     private Vector3 movementVector;
+
+    //horizontal
+    float horizontalMovment;
+
+    //vertical
+    float verticalMovement;
+
+    //foward
+    float fowardMovement;
+
+
     private float balance = 0.0f;
-    private float balanceAcceleration;
+    private float balanceMinorAcceleration = 1.0075f;
+    private float balanceModerateAcceleration = 1.01f;
+    private float balanceMajorAcceleration = 1.0125f;
+    public float balanceMax = 90;
+    private Transform sharkInitTransform;
+
+    //-----Jump
+    private float jumpInput;
+    private float jumpPower;
+    private float jumpMax = 90.0f;
+    private float jumpIncrement = 1.0f;
 
 
-    // Start is called before the first frame update
     void Start()
     {
         //-----Initialise Object Reference
         myCamera = GetComponentInChildren<Camera>();
-        player = GameObject.Find("/MainCharacter/Player");
+        tony = GameObject.Find("/MainCharacter/Player");
         shark = GameObject.Find("/MainCharacter/Shark");
+        sharkInitTransform = shark.transform;
+        characterController = this.GetComponent<CharacterController>();
+
+        balanceSlider.minValue = -balanceMax;
+        balanceSlider.maxValue = balanceMax;
     }
 
     // Update is called once per frame
@@ -31,22 +60,86 @@ public class PlayerControl : MonoBehaviour
     {
         GetInput();
         ProcessMovement();
+
+        balanceSlider.value = balance;
     }
 
     void GetInput()
     {
         balanceInput = Input.GetAxis("Horizontal");
+        jumpInput = 0;
+        if (Input.GetKey("space")) jumpInput = 1;
+        if (Input.GetKeyUp("space")) jumpInput = -1;
+        
     }
 
     void ProcessMovement()
     {
+        balance += balanceInput;
+        balance *= 1.01f;
 
-        balance = balanceInput;
+
+        //-----Minor Acceleration
+        if (balance > balanceMax / 4 || balance < balanceMax / 4)
+        {
+            balance *= balanceMinorAcceleration;
+        }
+
+        //-----Moderate Acceleration
+        if (balance > balanceMax / 2 || balance < balanceMax / 2)
+        {
+            balance *= balanceModerateAcceleration;
+        }
+
+        //-----Major Acceleration
+        if (balance > balanceMax || balance < -balanceMax)
+        {
+            balance = 0;
+        }
+        else if(balance > balanceMax - balanceMax/4 || balance < - balanceMax + balanceMax/4)
+        {
+            balance *= balanceMajorAcceleration;
+        }
+
+
+        //horizontal
+        horizontalMovment = balance/2;
+
+        //vertical
+        if (!characterController.isGrounded) verticalMovement -= 0.8f;
+        else verticalMovement = 0;
+
+        if (Input.GetKey("space") && jumpPower < jumpMax && characterController.isGrounded)
+        {
+            jumpPower += jumpIncrement;
+            
+        }
+        if (Input.GetKeyUp("space"))
+        {
+            verticalMovement = jumpPower;
+            jumpPower = 0;
+        }
+
+
         
+
         
-        player.transform.RotateAround(shark.transform.position, shark.transform.up, balance);
-        print(player.transform.rotation);
 
 
+        movementVector = Vector3.right * horizontalMovment;
+        movementVector += Vector3.forward * fowardMovement;
+        movementVector += Vector3.up * verticalMovement;
+
+        characterController.Move(movementVector * Time.deltaTime);
+
+        //shark
+        shark.transform.rotation = sharkInitTransform.rotation;
+        //shark.transform.Rotate(this.transform.right, -balance/200);
+
+        //tony
+        tony.transform.SetPositionAndRotation(this.transform.position + new Vector3(0.0f, 1.0f, 0.0f), this.transform.rotation);
+        tony.transform.RotateAround(shark.transform.position, shark.transform.up, 0.0f + -balance);
     }
+
+    
 }
