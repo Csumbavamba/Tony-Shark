6a2d0d8f2a533dd81edc045b4bd5fda79d987f83
Zsombor Pirok <zsombor.pirok@gmail.com>
2019-03-12 18:48:14 +1300

    Menu Working - WIP

---
+++ Tony Shark/Assets/UI/SceneLoader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private void Awake()
+    {
+        // Check if this object exists
+        int numberOfSceneLoaders = FindObjectsOfType<SceneLoader>().Length;
+
+        // Destroy object if there is more than one
+        if (numberOfSceneLoaders > 1)
+        {
+            Destroy(gameObject);
+        }
+        // Otherwise keep it
+        else
+        {
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("Start Menu");
+    }
+
+    public void LoadControls()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("Game"); // TODO Create Scene
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
