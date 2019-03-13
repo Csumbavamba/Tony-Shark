4dfbd3f95b74ba7e403a496b97a6adc0849636de
LOuis Cresswell <louiscresswell33@gmail.com>
2019-03-13 17:34:55 +1300

    waves have a collider, sort of

---
+++ Tony Shark/Assets/mesh.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mesh : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    MeshCollider c;
+    SkinnedMeshRenderer m;
+    
+    void Start()
+    {
+        c = GetComponent<MeshCollider>();
+        m = GetComponent<SkinnedMeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        c.enabled = false;
+        c.sharedMesh = m.sharedMesh;
+        c.enabled = true;
+    }
+}
